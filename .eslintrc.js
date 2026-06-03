a5f4c51e31b4cd0fe1992874d4dce81ed45863f4
voj-tech-j <jouzavojtech@gmail.com>
2026-06-03 11:03:09 +0200

    fix: drop runtime fs/path access for n8n Cloud

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,4 +8,14 @@ module.exports = {
 	plugins: ['n8n-nodes-base'],
 	extends: ['plugin:n8n-nodes-base/community'],
 	ignorePatterns: ['dist/**'],
+	overrides: [
+		{
+			// JS config files are not part of tsconfig.json, so disable the
+			// type-aware parser for them to avoid "parserOptions.project" errors.
+			files: ['*.js'],
+			parserOptions: {
+				project: null,
+			},
+		},
+	],
 };
